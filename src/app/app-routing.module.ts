9941f9b60fa46a925b20a983fe94495b0fa12446
Luis Manresa <luis.manresa@fabrick-io.com>
2021-02-26 20:41:40 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,50 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {AuthGuard} from './services/front/auth.guard';
+import {DashboardComponent} from './modules/dashboard/dashboard.component';
+import {ReportsComponent} from './modules/reports/reports.component';
+import {CitiesComponent} from './modules/cities/cities.component';
+import {UsersComponent} from './modules/users/users.component';
+import {LoginComponent} from './pages/login/login.component';
+import {NotFoundComponent} from './pages/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent, canActivate: [AuthGuard], children: [
+      {path: '', pathMatch: 'full', redirectTo: '/dashboard'},
+      {path: 'dashboard', component: DashboardComponent},
+      {
+        path: 'reports', children: [
+          {path: '', component: ReportsComponent},
+          {
+            path: ':interventionId/orders/:orderId',
+            component: ReportsComponent
+          }
+        ]
+      },
+      {
+        path: 'cities', children: [
+          {path: '', component: CitiesComponent},
+          {path: ':id', component: CitiesComponent},
+        ]
+      },
+      {
+        path: 'users', children: [
+          {path: '', component: UsersComponent},
+          {path: ':id', component: UsersComponent},
+        ]
+      },
+    ]
+  },
+  {path: 'login', component: LoginComponent},
+  {path: 'not-found', component: NotFoundComponent},
+  {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { ReportsComponent } from './modules/reports/reports.component';
+import { CitiesComponent } from './modules/cities/cities.component';
+import { UsersComponent } from './modules/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NotFoundComponent,
+    DashboardComponent,
+    ReportsComponent,
+    CitiesComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
