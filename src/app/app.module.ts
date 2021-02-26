548404ff0f26b49a5acaba8f8e879bc9da426df5
Luis Manresa <luis.manresa@fabrick-io.com>
2021-02-26 21:01:43 +0100

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,46 @@ import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { ReportsComponent } from './modules/reports/reports.component';
 import { CitiesComponent } from './modules/cities/cities.component';
 import { UsersComponent } from './modules/users/users.component';
+import { MenuComponent } from './pages/home/menu/menu.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSlideToggleModule, MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule, MatTooltipModule,
+  MatTreeModule
+} from '@angular/material';
+import {SatDatepickerModule} from 'saturn-datepicker';
+import {ChartsModule} from 'ng2-charts';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -20,11 +60,56 @@ import { UsersComponent } from './modules/users/users.component';
     DashboardComponent,
     ReportsComponent,
     CitiesComponent,
-    UsersComponent
+    UsersComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatProgressSpinnerModule,
+    MatSlideToggleModule,
+    MatChipsModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    MatDialogModule,
+    MatAutocompleteModule,
+    MatTreeModule,
+    MatSortModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatSnackBarModule,
+    MatProgressBarModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    DragDropModule,
+    MatRadioModule,
+    MatStepperModule,
+    MatBadgeModule,
+    NgxMaterialTimepickerModule,
+    SatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
